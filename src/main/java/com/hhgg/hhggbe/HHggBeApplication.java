6d40f66dc0f4929e32ddf8dc8c6f36213cf8111e
hwi3721 <lsh3721@gmail.com>
2023-01-25 18:00:28 +0900

    first commit

---
+++ src/main/java/com/hhgg/hhggbe/HHggBeApplication.java
@@ -0,0 +1,13 @@
+package com.hhgg.hhggbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHggBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HHggBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhgg/hhggbe/HHggBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhgg.hhggbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHggBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
