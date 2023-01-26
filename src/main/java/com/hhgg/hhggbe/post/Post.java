8e1ee1ef8250ee3691eade625c1305a5ce145010
hwi3721 <lsh3721@gmail.com>
2023-01-26 12:00:56 +0900

    post기본작업 임시저장

--- src/main/java/com/hhgg/hhggbe/post/Post.java
+++ src/main/java/com/hhgg/hhggbe/post/Post.java
@@ -1,11 +1,42 @@
 package com.hhgg.hhggbe.post;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hhgg.hhggbe.comment.Comment;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
+@RequiredArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long postId;
 
+    private String title;
+    private String content;
+    private String imageUrl;
+
+//    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+//    @JsonIgnore
+//    private List<Comment> comment = new ArrayList<>();
+    private boolean isDelete;
+
+    public void PostPatch(PostRequestDto postRequestDto, String imageUrl){
+        this.title = (postRequestDto.getTitle() == null) ? this.getTitle() : postRequestDto.getTitle();
+        this.content = (postRequestDto.getContent() == null) ? this.getContent() : postRequestDto.getContent();
+        this.imageUrl = (imageUrl == null) ? this.getImageUrl() : imageUrl;
+    }
+    public void PostPatchNoImage(PostRequestDto postRequestDto) {
+        this.title = (postRequestDto.getTitle() == null) ? this.getTitle() : postRequestDto.getTitle();
+        this.content = (postRequestDto.getContent() == null) ? this.getContent() : postRequestDto.getContent();
+        this.imageUrl = this.getImageUrl();
+    }
+    public void PostDelete(){
+        this.isDelete = true;
+    }
 }
---
+++ src/main/java/com/hhgg/hhggbe/post/PostRequestDto.java
@@ -0,0 +1,19 @@
+package com.hhgg.hhggbe.post;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostRequestDto {
+    private String title;
+    private String content;
+
+////    @Override
+//    public String toString() {
+//        return "ArticleForm{"+
+//                "title='" + title + '\'' +
+//                "content='" + content +'\'' +
+//                '}';
+//    }
+}
